167bf6d5cc493a01f07274d275bc38cafb6ad3f5
Daniel Hu <dan3455527@gmail.com>
2022-03-04 12:34:35 +0800

    3_7.c fix

--- hw3_solution/3_7.c
+++ hw3_solution/3_7.c
@@ -1,31 +1,21 @@
-#include <stdio.h>
+# include <stdio.h>
 
-int main(void)
-{
-    float pi, dominator, sum_odd = 0, sum_even = 0;
-    int odd_count, even_count, terms;
-    printf("enter the terms to calculate:\n");
-    scanf("%d", &terms);
+int main(void){
+    double pi = 0, precision, tolerance ;
+    int terms = 1;
+    int terms_limit;
 
-    odd_count = terms / 2 + terms % 2;
-    even_count = terms / 2;
+    printf("enter run terms limits:\n");
+    scanf("%d", &terms_limit);
 
-    // odd element
-    dominator = 1;
-    for (int count = 0; count < odd_count; count++)
-    {
-        sum_odd = sum_odd + (4 / dominator);
-        dominator += 4;
+    while(terms <= terms_limit){
+        if (terms % 2 == 0){
+            pi = pi - 4.0 / (2 * terms - 1);
+        }
+        else{
+            pi = pi + 4.0 / (2 * terms - 1);
+        }
+        printf("terms : %-5d %15.10lf\n", terms, pi);
+        terms ++;
     }
-    // even element
-    dominator = 3;
-    for (int count = 0; count < even_count; count++)
-    {
-        sum_even = sum_even - (4 / dominator);
-        dominator += 4;
-    }
-    pi = sum_even + sum_odd;
-    printf("term : %-6d\npi : %10f\n", terms, pi);
 }
-// first 3.14  : 119
-// first 3.141 :
